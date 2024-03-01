95567d693fbc4f163cd8a216feefe0975d5a2fbe
Ekaterina Bondareva <1978231@bk.ru>
2024-03-01 18:13:58 +0500

    Created State in Add and created component "Header"

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,20 @@
-import React, { Component } from "react";
+import React from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { useState } from "react";
 
-import MainPage from "./MainPage";
+import Header from "./Header";
 
-class App extends Component {
-  render() {
-    return (
-      <Router>
-        <Routes>
-          <Route path="/" element={<MainPage />}></Route>
-        </Routes>
-      </Router>
-    );
-  }
+export default function App() {
+  const [saveGoodsLength, setSaveGoodsLength] = useState<number>(0);
+  const [goodsInBag, setGoodsInBag] = useState<number>(0);
+
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route path="/" element={<Header />}></Route>
+        </Route>
+      </Routes>
+    </Router>
+  );
 }
-
-export default App;
