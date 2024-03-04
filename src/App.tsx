3815dc5603cf2727f9c2c3089ba05b9b11359e0b
Ekaterina Bondareva <1978231@bk.ru>
2024-03-04 21:48:41 +0500

    Header (tsc and css)

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,19 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { useState } from "react";
 
-import Header from "./Header";
+import { Header } from "./Header";
 
 export default function App() {
   const [saveGoodsLength, setSaveGoodsLength] = useState<number>(0);
   const [goodsInBag, setGoodsInBag] = useState<number>(0);
 
   return (
-    <Router>
+    <>
+      <Header savedCount={saveGoodsLength} bagCount={goodsInBag} />
       <Routes>
-        <Route path="/" element={<App />}>
-          <Route path="/" element={<Header />}></Route>
-        </Route>
+        <Route path="/" element={<App />}></Route>
       </Routes>
-    </Router>
+    </>
   );
 }
--- src/Header.tsx
+++ src/Header.tsx
@@ -1,7 +1,34 @@
-import { useState } from "react";
+import { Link } from "react-router-dom";
+import "./Header.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Button from "react-bootstrap/Button";
+import ButtonGroup from "react-bootstrap/ButtonGroup";
+import ButtonToolbar from "react-bootstrap/ButtonToolbar";
 
-export default function MainPage() {
-  const [saveGoodsLength, setSaveGoodsLength] = useState<number>(0);
-  const [goodsInBag, setGoodsInBag] = useState<number>(0);
-  return <div className="mainPage"></div>;
+import Container from "react-bootstrap/Container";
+import Navbar from "react-bootstrap/Navbar";
+interface Props {
+  savedCount: number;
+  bagCount: number;
 }
+
+export const Header: React.FC<Props> = ({ savedCount, bagCount }) => (
+  <Navbar bg="primary" data-bs-theme="dark">
+    <Container>
+      <Navbar.Brand href="#home">
+        <Link to="/" className="header__title">
+          Магазин Кружек
+        </Link>
+      </Navbar.Brand>
+
+      <ButtonGroup className="me-2" aria-label="First group">
+        <Link to="/saved" className="header__saved">
+          <Button variant="secondary">Избранное ({savedCount})</Button>
+        </Link>
+        <Link to="/bag" className="header__bag">
+          <Button variant="secondary">Корзина({bagCount})</Button>
+        </Link>
+      </ButtonGroup>
+    </Container>
+  </Navbar>
+);
