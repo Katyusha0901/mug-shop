8d168eacf1d7a897a51a5d5586b40f7fd4a520e9
Ekaterina Bondareva <1978231@bk.ru>
2024-03-09 15:58:40 +0500

    added functionality of adding goods in bag and in saved goods

--- src/MainPage/Product.tsx
+++ src/MainPage/Product.tsx
@@ -2,12 +2,22 @@ import { Good } from "./Goods";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { GoodsContext } from "../Context/GoodsContextProvider";
+import { useState, useContext } from "react";
+import "./Product.css";
 
 interface Props {
   productInformation: Good;
 }
 
 export const Product: React.FC<Props> = ({ productInformation }) => {
+  const { savedGoods, setSavedGoods, goodsInBag, setGoodsInBag } = useContext<{
+    savedGoods: number[];
+    setSavedGoods: (array: number[]) => void;
+    goodsInBag: number[];
+    setGoodsInBag: (array: number[]) => void;
+  }>(GoodsContext);
+
   return (
     <div className="product">
       <Card style={{ width: "18rem" }}>
@@ -16,11 +26,26 @@ export const Product: React.FC<Props> = ({ productInformation }) => {
           <Card.Title>{productInformation.name}</Card.Title>
           <Card.Text>{productInformation.price} руб.</Card.Text>
           <div className="product__buttons">
-            {" "}
-            <Button variant="primary">
-              {/* {isSaved ?<img src=""> : <img img src="">}  */}
+            <Button
+              variant="primary"
+              onClick={() =>
+                !savedGoods.includes(productInformation.id)
+                  ? setSavedGoods([...savedGoods, productInformation.id])
+                  : setSavedGoods([...savedGoods])
+              }
+            >
+              Сохранить
             </Button>
-            <Button variant="primary">В корзину</Button>
+            <Button
+              variant="primary"
+              onClick={() =>
+                !goodsInBag.includes(productInformation.id)
+                  ? setGoodsInBag([...goodsInBag, productInformation.id])
+                  : setGoodsInBag([...goodsInBag])
+              }
+            >
+              В корзину
+            </Button>
           </div>
         </Card.Body>
       </Card>
