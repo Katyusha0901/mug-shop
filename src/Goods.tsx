59a8e29b12fc8548c90e7d5eb0373d5ed03bafd6
Ekaterina Bondareva <1978231@bk.ru>
2024-03-11 22:26:27 +0500

    added products and corrected flex-wrap

--- src/Goods.tsx
+++ src/Goods.tsx
@@ -1,6 +1,9 @@
 import first from "./images/1.webp";
 import second from "./images/2.jpg";
 import third from "./images/3.jpg";
+import fourth from "./images/4.jpg";
+import fifth from "./images/5.jpg";
+import sixth from "./images/6.jpg";
 
 export interface Good {
   id: number;
@@ -29,11 +32,27 @@ export const Goods: Good[] = [
     isWithHandle: true,
   },
   {
-    id: 1,
+    id: 2,
     name: "Стакан",
     image: third,
     description: "Стакан, чтобы пить достаточно воды каждый день",
     price: 200,
+    isWithHandle: false,
+  },
+  {
+    id: 3,
+    name: "Рог для вина",
+    image: fourth,
+    description: "Отдохни по-грузински",
+    price: 600,
+    isWithHandle: false,
+  },
+  {
+    id: 4,
+    name: "Бокал для вина Бордо",
+    image: fifth,
+    description: "Отдохни после напряженного дня",
+    price: 300,
     isWithHandle: false,
   },
 ];
