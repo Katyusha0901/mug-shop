18a1bf67d37b9870a5f30d8b5ad859d93352d612
Ekaterina Bondareva <1978231@bk.ru>
2024-03-11 13:07:16 +0500

    added closeButton

--- src/SavedProduct.tsx
+++ src/SavedProduct.tsx
@@ -20,30 +20,44 @@ export const SavedProduct: React.FC<Props> = ({ productInformation }) => {
   }>(GoodsContext);
 
   return (
-    <Card style={{ width: "18rem" }}>
-      <Link key={productInformation.id} to={`/${productInformation.id}`}>
-        <Card.Img variant="top" src={productInformation.image} />
-      </Link>
-      <Card.Body>
-        <div className="saved-product__card-body">
-          <Link key={productInformation.id} to={`/${productInformation.id}`}>
-            <Card.Title>{productInformation.name}</Card.Title>
-          </Link>
-          <Card.Text>{productInformation.price} руб.</Card.Text>
-          <div className="product__buttons">
-            <Button
-              variant="primary"
-              onClick={() =>
-                !goodsInBag.includes(productInformation.id)
-                  ? setGoodsInBag([...goodsInBag, productInformation.id])
-                  : setGoodsInBag([...goodsInBag])
-              }
-            >
-              В корзину
-            </Button>
-          </div>
+    <div className="saved-product">
+      <Card style={{ width: "18rem" }}>
+        <div className="saved-product__close-button">
+          <Button
+            variant="primary"
+            onClick={() =>
+              setSavedGoods(
+                savedGoods.filter((id) => id !== productInformation.id)
+              )
+            }
+          >
+            ×
+          </Button>
         </div>
-      </Card.Body>
-    </Card>
+        <Link key={productInformation.id} to={`/${productInformation.id}`}>
+          <Card.Img variant="top" src={productInformation.image} />
+        </Link>
+        <Card.Body>
+          <div className="saved-product__card-body">
+            <Link key={productInformation.id} to={`/${productInformation.id}`}>
+              <Card.Title>{productInformation.name}</Card.Title>
+            </Link>
+            <Card.Text>{productInformation.price} руб.</Card.Text>
+            <div className="product__buttons">
+              <Button
+                variant="primary"
+                onClick={() =>
+                  !goodsInBag.includes(productInformation.id)
+                    ? setGoodsInBag([...goodsInBag, productInformation.id])
+                    : setGoodsInBag([...goodsInBag])
+                }
+              >
+                В корзину
+              </Button>
+            </div>
+          </div>
+        </Card.Body>
+      </Card>
+    </div>
   );
 };
