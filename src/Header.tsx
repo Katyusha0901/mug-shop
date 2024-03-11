0ce03940f0c8b8ad8f4c7e7ca8240cf28dba70f2
Ekaterina Bondareva <1978231@bk.ru>
2024-03-11 21:11:45 +0500

    changed header

--- src/Header.tsx
+++ src/Header.tsx
@@ -7,6 +7,7 @@ import Container from "react-bootstrap/Container";
 import Navbar from "react-bootstrap/Navbar";
 import { useContext } from "react";
 import { GoodsContext } from "./Context/GoodsContextProvider";
+import Nav from "react-bootstrap/Nav";
 
 export function Header() {
   const { savedGoods, goodsInBag } = useContext<{
@@ -16,23 +17,20 @@ export function Header() {
     setGoodsInBag: (array: number[]) => void;
   }>(GoodsContext);
   return (
-    <Navbar bg="primary" data-bs-theme="dark">
-      <Container>
-        <Navbar.Brand href="#home">
-          <Link to="/mug-shop" className="header__title">
-            Магазин Кружек
-          </Link>
-        </Navbar.Brand>
-
-        <ButtonGroup className="me-2" aria-label="First group">
-          <Link to="/saved-products" className="header__saved">
-            <Button variant="secondary">Избранное ({savedGoods.length})</Button>
-          </Link>
-          <Link to="/bag" className="header__bag">
-            <Button variant="secondary">Корзина({goodsInBag.length})</Button>
-          </Link>
-        </ButtonGroup>
-      </Container>
-    </Navbar>
+    <div className="header">
+      <div className="header__title">
+        <Link to="/mug-shop" className="header__text">
+          Магазин Кружек
+        </Link>
+      </div>
+      <div className="buttons">
+        <Link to="/saved-products" className="buttons__saved">
+          Избранное ({savedGoods.length})
+        </Link>
+        <Link to="/bag" className="buttons__bag">
+          Корзина({goodsInBag.length})
+        </Link>
+      </div>
+    </div>
   );
 }
