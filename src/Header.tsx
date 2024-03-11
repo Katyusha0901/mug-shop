560f7854077c42296fb17227c68971d45e5366c4
Ekaterina Bondareva <1978231@bk.ru>
2024-03-11 22:57:37 +0500

    made code beautiful

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,13 +1,8 @@
 import { Link } from "react-router-dom";
 import "./Header.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Button from "react-bootstrap/Button";
-import ButtonGroup from "react-bootstrap/ButtonGroup";
-import Container from "react-bootstrap/Container";
-import Navbar from "react-bootstrap/Navbar";
 import { useContext } from "react";
 import { GoodsContext } from "./Context/GoodsContextProvider";
-import Nav from "react-bootstrap/Nav";
 
 export function Header() {
   const { savedGoods, goodsInBag } = useContext<{
@@ -16,6 +11,7 @@ export function Header() {
     goodsInBag: number[];
     setGoodsInBag: (array: number[]) => void;
   }>(GoodsContext);
+
   return (
     <div className="header">
       <div className="header__title">
@@ -23,10 +19,12 @@ export function Header() {
           Магазин Кружек
         </Link>
       </div>
+
       <div className="buttons">
         <Link to="/saved-products" className="buttons__saved">
           Избранное ({savedGoods.length})
         </Link>
+
         <Link to="/bag" className="buttons__bag">
           Корзина({goodsInBag.length})
         </Link>
