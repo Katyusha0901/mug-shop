b940c8565f2e644b34433b0142e17738c04e76b9
Ekaterina Bondareva <1978231@bk.ru>
2024-03-12 12:48:00 +0500

    added swich instead of if

--- src/MainPage/MainPage.tsx
+++ src/MainPage/MainPage.tsx
@@ -7,34 +7,28 @@ import { Product } from "./Product";
 
 export function MainPage() {
   const [visibleMugsWithHandle, setVisibleMugsWithHandle] = useState<boolean>();
-  const [visibleMugsWithoutHandle, setVisibleMugsWithoutHandle] =
-    useState<boolean>();
 
   function showAllGoods() {
-    setVisibleMugsWithHandle(false);
-    setVisibleMugsWithoutHandle(false);
+    setVisibleMugsWithHandle(undefined);
   }
 
   function showGoodsWithHandle() {
     setVisibleMugsWithHandle(true);
-    setVisibleMugsWithoutHandle(false);
   }
 
   function showGoodsWithoutHandle() {
-    setVisibleMugsWithoutHandle(true);
     setVisibleMugsWithHandle(false);
   }
 
   function defineVisibleProducts() {
     const visibleGoodsArr = Goods.filter((product) => {
-      if (visibleMugsWithHandle && visibleMugsWithoutHandle) {
-        return product;
-      } else if (visibleMugsWithHandle) {
-        return product.isWithHandle;
-      } else if (visibleMugsWithoutHandle) {
-        return !product.isWithHandle;
-      } else if (!visibleMugsWithHandle && !visibleMugsWithoutHandle) {
-        return product;
+      switch (visibleMugsWithHandle) {
+        case undefined:
+          return product;
+        case true:
+          return product.isWithHandle;
+        case false:
+          return !product.isWithHandle;
       }
     });
 
