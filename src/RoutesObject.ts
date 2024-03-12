9652f3f512f2f6fb09e275795a5c188838f0088f
Ekaterina Bondareva <1978231@bk.ru>
2024-03-12 12:22:27 +0500

    added type in routes object

--- src/RoutesObject.ts
+++ src/RoutesObject.ts
@@ -1,4 +1,11 @@
-export const RoutesObject = {
+interface RoutesType {
+  mainPage: string;
+  savedProducts: string;
+  bag: string;
+  productPage: string;
+}
+
+export const RoutesObject: RoutesType = {
   mainPage: "/",
   savedProducts: "/saved-products",
   bag: "/bag",
